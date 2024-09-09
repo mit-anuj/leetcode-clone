a9b3669e49e85ded3ed44fe5f6e3aae8593bec57
mit-anuj <anujmittal0040@gmail.com>
2024-09-09 18:21:12 +0530

    added the toolbar for the problem page and added the additional functionality (timer,arrow keys etc)

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Topbar from "@/components/Topbar/Topbar";
 export default function Home() {
   return (
     <main className="bg-dark-layer-2 min-h-screen">
-      <Topbar/>
+      <Topbar problemPage={false}/>
       <h1 className="text-2xl text-center text-gray-700 dark:text-gray-400 font-medium uppercase mt-10 mb-5">
         &ldquo; QUALITY OVER QUANTITY &ldquo; 
       </h1>
---
+++ src/pages/problems/[pid].jsx
@@ -0,0 +1,12 @@
+import Topbar from '@/components/Topbar/Topbar'
+import React from 'react'
+
+const PrblemPage = () => {
+  return (
+    <div>
+      <Topbar problemPage={true}/>
+    </div>
+  )
+}
+
+export default PrblemPage
